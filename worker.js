1320ec041557b68c186648571f660d9c8f562661
VidBye <vidbye@gmail.com>
2025-05-25 00:49:53 +0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,12 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
-const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
-const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_SECRET = "SIA_SECRET"; // Insert a powerful secret text and keep it safe.
-const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
+const BOT_TOKEN = "8176280350:AAFkcWrohhfd5QgLfWQJUkgM9Ozg25_5uxU";
+const BOT_WEBHOOK = "/endpoint";
+const BOT_SECRET = "123";
+const BOT_OWNER = 7568164710;
+const BOT_CHANNEL = -1002426753544;
+const SIA_SECRET = "Gg";
+const PUBLIC_BOT = true;
 
 // ---------- Do Not Modify ---------- // 
 
